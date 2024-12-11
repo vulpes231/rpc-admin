4b6e98623b466c4a97d3d9bf68c93d4237a6af4a
Vulpes231 <whykayshot@gmail.com>
2024-12-11 23:35:40 +0100

    unused

--- src/features/verifySlice.js
+++ src/features/verifySlice.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { devServer, getAccessToken, liveServer, sendError } from "../constants";
 import axios from "axios";
